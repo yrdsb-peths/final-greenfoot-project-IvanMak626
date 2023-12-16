1f05cbc041f6c3de767badf5ecfffcf6e93bf26a
IvanMak626 <341034262@gapps.yrdsb.ca>
2023-12-15 22:29:42 -0500

    made fireball face left and right one corresponding side of actor

--- Fireball.java
+++ Fireball.java
@@ -10,6 +10,8 @@ public class Fireball extends Actor
 {
     GreenfootImage[] idleRight = new GreenfootImage[8];
     GreenfootImage[] idleLeft = new GreenfootImage[8];
+    GreenfootImage[] idleUp = new GreenfootImage[8];
+    GreenfootImage[] idleDown = new GreenfootImage[8];
     
     String facing = "right";
     SimpleTimer animationTimer = new SimpleTimer();
@@ -29,6 +31,19 @@ public class Fireball extends Actor
             idleLeft[i].scale(100, 100);
         }
         
+        for(int i = 0; i < idleUp.length; i++)
+        {
+            idleUp[i] = new GreenfootImage("images/fireball_1/fireball" + (i+16) + ".png");
+            idleUp[i].scale(100, 100);
+        }
+        
+        for(int i = 0; i < idleDown.length; i++)
+        {
+            idleDown[i] = new GreenfootImage("images/fireball_1/fireball" + (i+16) + ".png");
+            idleDown[i].mirrorVertically();
+            idleDown[i].scale(100, 100);
+        }
+        
         animationTimer.mark();
         setImage(idleRight[0]);
     }
@@ -46,25 +61,52 @@ public class Fireball extends Actor
         {
             setImage(idleRight[imageIndex]);
             imageIndex = (imageIndex + 1) % idleRight.length;
-        } else {
+        } 
+        else if(facing.equals("left"))
+        {
             setImage(idleLeft[imageIndex]);
             imageIndex = (imageIndex + 1) % idleLeft.length;
         }
+        else if(facing.equals("up"))
+        {
+            setImage(idleUp[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleUp.length;
+        }
+        else if(facing.equals("down"))
+        {
+            setImage(idleDown[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleDown.length;
+        }
     }
+    
     
     public void act()
     {
-        if(Greenfoot.isKeyDown("a"))
+        MouseInfo mouseInfo = Greenfoot.getMouseInfo();
+        if(mouseInfo == null)
         {
-            move(-2);
-            facing = "left";
+            System.out.println("wrnog");
         }
-        else if(Greenfoot.isKeyDown("d"))
+        else
         {
-            move(2);
-            facing = "right";
+            if(mouseInfo.getX() > 300)
+            {
+                facing = "right";
+            }
+            else if(mouseInfo.getX() < 300)
+            {
+                facing = "left";
+            }
+            else if(mouseInfo.getY() > 300)
+            {
+                facing = "down";
+            }
+            else if(mouseInfo.getY() < 300)
+            {
+                facing = "up";
+            }
         }
-        
+    
         animateFireBall();
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -8,20 +8,19 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-    /**
-     * Constructor for objects of class MyWorld.
-     * 
-     */
+    public Knight knight = new Knight();
+    
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
         
-        Knight knight = new Knight();
         addObject(knight, 300, 300);
         
-        Fireball fireball = new Fireball();
-        addObject(fireball, knight.getX(), knight.getY());
+        MouseInfo mouseInfo = Greenfoot.getMouseInfo();
         
+        Fireball fireball = new Fireball();
+        
+        addObject(fireball, knight.getX(), knight.getY());
     }
 }
